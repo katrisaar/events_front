1af609c406a0c74691c961bb949458a0825f4224
katrisaar <katrisaar0@gmail.com>
2023-05-17 17:13:39 +0300

    logini front errorini ilusti tehtud. logini backi pool veel sidumata

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">Avaleht</router-link> |
+    <router-link to="/login">Logi sisse</router-link>
   </nav>
   <router-view/>
 </template>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="container">
+        <AlertDanger :message="message"/>
+        <div class="row justify-content-center">
+            <div class="col col-3">
+                <div class="input-group mb-3">
+                    <span class="input-group-text">Kasutajanimi</span>
+                    <input v-model="username" type="text" class="form-control" id="username">
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col col-3">
+                <div class="input-group mb-3">
+                    <span class="input-group-text" id="inputGroup-sizing-default">Parool</span>
+                    <input v-model="password" type="text" class="form-control" id="password">
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <button @click="login" class="btn btn-primary" type="submit">Logi sisse</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import AlertDanger from "@/components/alert/AlertDanger.vue";
+
+export default {
+    name: "LoginView",
+    components: {AlertDanger},
+    data() {
+        return {
+            username: '',
+            password: '',
+            message: ''
+        }
+    },
+    methods: {
+        login() {
+            this.message = ''
+            if (this.username === '' || this.password === '') {
+                this.message = 'Oot..oot..nii küll ei saa luurele minna. Täida ikka mõlemad väljad ära!'
+            }
+        },
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
