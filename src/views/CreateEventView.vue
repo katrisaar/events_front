448e2b6c4c95791c430cb98cde20a9308df1c103
ilona <ilona.marken@yahoo.com>
2023-05-23 10:04:40 +0300

    CreateEvent in progress + dropdown components

--- src/views/CreateEventView.vue
+++ src/views/CreateEventView.vue
@@ -1,12 +1,98 @@
 <template>
     <div class="container">
         <div class="row">
-            <div class="col">
-                Column
+            <div class="col mb-5">
+                <div class="input-group flex-nowrap">
+                    <span class="input-group-text" id="addon-wrapping">Mis</span>
+                    <input type="text" class="form-control" aria-label="Username" aria-describedby="addon-wrapping">
+                </div>
             </div>
             <div class="col">
-                Column
-                //TODO: "Tee uus üritus" nupp Ürituste lehele
+                Lisa pilt
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                Algus
+            </div>
+            <div class="col">
+                Kell
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                Lõpp
+            </div>
+            <div class="col">
+                Kell
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                Piirkond
+            </div>
+            <div class="col">
+                Lisa uus piirkond
+            </div>
+            <div class="col">
+                Lisa piirkond nupp
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                Valdkond
+            </div>
+            <div class="col">
+                Lisa uus valdkond
+            </div>
+            <div class="col">
+                Valdkonna lisamise nupp
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                Viimane aeg registreeruda
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                Minimaalne osalejate arv
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                Maksimaalne osalejate arv
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                Osalemistasu
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                Täpne aadress
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                Aadressi koordinaatide sisestamise nupp
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                Aadressi kaardil määramise aadress
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <div class="row">
+                    Kirjeldus
+                </div>
+                <div class="form-floating">
+                    <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 100px"></textarea>
+                    <label for="floatingTextarea2"></label>
+                </div>
             </div>
         </div>
     </div>
