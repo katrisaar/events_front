e8cce3f62471e6dcf8776cf0582f74cec5050603
katrisaar <katrisaar0@gmail.com>
2023-05-24 15:48:56 +0300

    Avalehe funktsionaalne pool valmis. Ilus veel ei ole.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,36 @@
 <template>
-  <div class="home">
-    See on avaleht, kus hetkel pole midagi
-  </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm">
+                <h5>Peatselt lõppeva registreerumisega üritused</h5>
+                <SoonToFinishEvents/>
+            </div>
+            <div class="col-sm">
+                <h5>Peatselt täis saavad üritused</h5>
+                <SoonToFillEvents/>
+            </div>
+            <div class="col-sm">
+                <h5>Värskeimalt lisatud üritused</h5>
+                <MostRecentEvents/>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
 // @ is an alias to /src
 
 import Modal from "@/components/modal/Modal.vue";
+import SoonToFinishEvents from "@/components/events/SoonToFinishEvents.vue";
+import SoonToFillEvents from "@/components/events/SoonToFillEvents.vue";
+import MostRecentEvents from "@/components/events/MostRecentEvents.vue";
 
 export default {
   name: 'HomeView',
   components: {
+      MostRecentEvents,
+      SoonToFillEvents,
+      SoonToFinishEvents,
       Modal
 
   }
